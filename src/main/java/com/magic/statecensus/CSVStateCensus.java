f898d7fe8094aaca603e89af6525c0aabb11c75b
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-06-14 13:59:05 +0530

    Add-loadStateCensusData takes filepath and load CSV data

---
+++ src/main/java/com/magic/statecensus/CSVStateCensus.java
@@ -0,0 +1,28 @@
+package com.magic.statecensus;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCensus {
+
+    @CsvBindByName(column = "State", required = true)
+    public String state;
+
+    @CsvBindByName(column = "Population", required = true)
+    public int population;
+
+    @CsvBindByName(column = "AreaInSqKm", required = true)
+    public int areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
+    public int densityPerSqKm;
+
+    @Override
+    public String toString() {
+        return "CSVStateCensus{" +
+                "state='" + state + '\'' +
+                ", population=" + population +
+                ", areaInSqKm=" + areaInSqKm +
+                ", densityPerSqKm=" + densityPerSqKm +
+                '}';
+    }
+}
--- src/main/java/com/magic/statecensus/StateCensusAnalyser.java
+++ src/main/java/com/magic/statecensus/StateCensusAnalyser.java
@@ -1,8 +1,33 @@
 package com.magic.statecensus;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class StateCensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Census State Analyser");
+    public static int numOfEntries = 0;
+
+    public int loadStateCensusData(String path) throws IOException {
+        Reader reader = Files.newBufferedReader(Paths.get(path));
+        CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder<CSVStateCensus>(reader).
+                withType(CSVStateCensus.class).withIgnoreLeadingWhiteSpace(true)
+                .build();
+        Iterator<CSVStateCensus> csvStateCensusIterator = csvToBean.iterator();
+        while (csvStateCensusIterator.hasNext()) {
+            numOfEntries++;
+            csvStateCensusIterator.next();
+        }
+        return numOfEntries;
+    }
+
+    public static void main(String[] args) throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        System.out.println(stateCensusAnalyser.loadStateCensusData("C:\\Users\\rajni\\Desktop\\StateCensusData.csv"));
 
     }
 }
